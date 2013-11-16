7ff0a903778cc6ef1624b6b03fc3ebaa3a26a400
Jorge Zaccaro <jorgezaccaro@gmail.com>
2013-11-16 13:39:03 -0500

    Rearranged folder structure under airport, aircraft and wingify folders.

---
+++ airport/airport.js
@@ -0,0 +1,27 @@
+/* NODE MODULES */
+
+/** LOCAL OBJECT 
+ * @property {} - 
+ */
+var LOCAL = {
+    
+};
+
+/** MODULE INTERFACE
+ *@method {function} - 
+ */
+module.exports = {
+    
+};
+
+/*----------------------------------------------------------------------------*/
+
+/** 
+ * @param
+ * @returns
+ */
+function () {
+    
+}
+
+// 
