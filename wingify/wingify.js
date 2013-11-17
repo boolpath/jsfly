33e29c2da398609c1cbab25164b8031083a80570
Jorge Zaccaro <jorgezaccaro@gmail.com>
2013-11-16 21:48:21 -0500

    Exposed the wingify module interface and added validation.

---
+++ wingify/wingify.js
@@ -0,0 +1,50 @@
+/* NODE MODULES */
+var exceptions = require('../exceptions');
+/** LOCAL OBJECT 
+ * @property {} - 
+ */
+var WINGIFY = {
+    validate: validate,
+    aircraft: require('../aircraft')
+};
+
+/** MODULE INTERFACE
+ * This module only exposes one function that can be called by invoking the module directly
+ */
+module.exports = wingify;
+
+
+/*----------------------------------------------------------------------------*/
+
+
+/** Wingify === transforming a piece of code into migratable/autonomous code
+ *          === to give it wings to fly from one server to another
+ * @param {object} options - Describes the attributes of the code to be wingified
+ * @param {function} code - The code to be wingified
+ * @returns {object} wingified - A version of the original code that is able to fly
+ */
+function wingify(options, code) {
+    // Validate the supplied code and options and return if invalid
+    if(!WINGIFY.validate(options, code)) { return; }
+    // Create a migratable/autonomous piece of code
+    var wingified = WINGIFY.aircraft.create(options, code);
+
+    return  {run: function () {}};
+}
+
+
+/** Validates the code to be wingified according to the specified options
+ * @param {object} options - Describes the attributes of the code to be wingified
+ * @param {function} code - The code to be wingified
+ * @returns {boolean}
+ */
+function validate(options, code) {
+    var valid = true, codeName;
+
+    if(!code) { throw exceptions.throwNew('no function'); }
+    codeName = code.name || code.tag || options.name;
+
+    if(!codeName) { throw exceptions.throwNew('unnamed function'); }
+
+    return valid;
+}
