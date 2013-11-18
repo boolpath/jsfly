bdba285f17cbd92d7565d0f46f588b7d1beef45d
Jorge Zaccaro <jorgezaccaro@gmail.com>
2013-11-18 14:47:15 -0500

    Added methods to handle access and calls to globals.

--- airspace/airspace.js
+++ airspace/airspace.js
@@ -1,17 +1,19 @@
 /* NODE MODULES */
-
+var exceptions = require('../exceptions');
 /** LOCAL OBJECT 
  * @property {} - 
  */
-var AIR = {
-    
+var AIRSPACE = {
+    calls: []
 };
 
 /** MODULE INTERFACE
  *@method {function} - Sets up the airspace by configuring the global variables and methods
  */
 module.exports = {
-    setup: setup
+    setup: setup,
+    prependCalls: prependCalls,
+    getGlobals: getGlobals
 };
 
 /*----------------------------------------------------------------------------*/
@@ -21,6 +23,39 @@ module.exports = {
  *  and to avoid redefinitions by wingified code
  * @returns
  */
-function setup() {
-    
+function setup(callsToPrepend) {
+    var prependCalls = callsToPrepend || [
+        'setTimeout', 
+        'clearTimeout',
+        'setInterval',
+        'clearInterval'
+    ];
+
+    prependCalls.forEach(function (call) {
+        AIRSPACE.calls.push(call);
+    });
+}
+
+/** 
+ * @param
+ * @returns
+ */
+function prependCalls(rawFunction) {
+    var stringFunction = rawFunction.toString().trim();
+    AIRSPACE.calls.forEach(function(call) {
+        stringFunction = stringFunction.replace(call, 'jsfly.'+call);
+    });
+    return stringFunction;
+}
+
+/** 
+ * @param
+ * @returns
+ */
+function getGlobals() {
+    var globals = {};
+
+    globals.setTimeout = setTimeout;
+
+    return globals;
 }
