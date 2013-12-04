9522a490111776f40c3772e9fc4f96540cf341a5
Jorge Zaccaro <jorgezaccaro@gmail.com>
2013-12-04 17:35:19 -0500

    Exposed the first method of the runway module to request permission to fly.

---
+++ airport/runway/runway.js
@@ -0,0 +1,26 @@
+/* NODE MODULES */
+var net = require('net');
+var exceptions = require('../../utils/exceptions');
+/** LOCAL OBJECT 
+ * @property {} - 
+ */
+var RUNWAY = {
+    
+};
+
+/** MODULE INTERFACE
+ *@method {function} - 
+ */
+module.exports = {
+    request: request
+};
+
+/*----------------------------------------------------------------------------*/
+
+/** 
+ * @param
+ * @returns
+ */
+function request(targetAirport, reply) {
+    
+}
