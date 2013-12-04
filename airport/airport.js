4063f720a6eb93f3ccc9f5f6afa4c4cdcb95713e
Jorge Zaccaro <jorgezaccaro@gmail.com>
2013-12-03 22:01:06 -0500

    Added validation and bindings to runway and tower modules.

--- airport/airport.js
+++ airport/airport.js
@@ -1,17 +1,23 @@
 /* NODE MODULES */
+var exceptions = require('../utils/exceptions');
+var eventEmitter = require('events').EventEmitter;
 
 /** LOCAL OBJECT 
  * @property {} - 
  */
 var AIRPORT = {
-    gates: {}
+    runway: require('./runway'),
+    tower: require('./tower'),
+    gates: {},
+
+    emitter: new eventEmitter()
 };
 
 /** MODULE INTERFACE
  *@method {function} create - Creates a JSFly server
  */
 module.exports = {
-    createServer: createServer,
+    create: create,
     addPlane: addPlane,
 
     gates: AIRPORT.gates
@@ -23,8 +29,22 @@ module.exports = {
  * @param
  * @returns
  */
-function createServer() {
-    
+function create(port, onReady) {
+    if (typeof port !== 'number') {
+        port = parseInt(port);
+        if (isNaN(port)) {
+            throw exceptions.throwNew('wrong port');
+        }
+    } else if (typeof onReady !== 'function') {
+        throw exceptions.throwNew('wrong onReady');
+    }
+
+    AIRPORT.tower.listen(port, function (tower) {
+        onReady(AIRPORT.emitter);
+        tower.on('ready', function () {
+            AIRPORT.emitter.emit('ready');
+        });
+    });
 }
 
 /** Adds
