f8c3a937d0c2f8deec43c1a1d5afba03f57fe95e
Jorge Zaccaro <jorgezaccaro@gmail.com>
2013-12-03 22:10:09 -0500

    Added exceptions to airport#create parameter validation.

--- utils/exceptions/exceptions.js
+++ utils/exceptions/exceptions.js
@@ -10,7 +10,11 @@ var EXCEPTIONS = {
     'no code': ['No code', 'jsfly.wingify() was called without supplying a function.'],
     'no function': ['No function', 'Parameter "code" in jsfly.wingify(code) must be a function.'],
     'nostring function': ['No string code', 'The name/tag/options.name parameters supplied to jsfly.wingify(function) must be a string.'],
-    'unnamed function': ['Unnamed code', 'The function supplied to jsfly.wingify(function) must be a named function or have a tag attribute.']
+    'unnamed function': ['Unnamed code', 'The function supplied to jsfly.wingify(function) must be a named function or have a tag attribute.'],
+
+    // 'wrong host': ['Wrong host', 'The "host" provided to jsfly.createServer(host, ...) is invalid.'],
+    'wrong port': ['Wrong port', 'The "port" provided to jsfly.createServer(port) is invalid.'],
+    'wrong onReady': ['Wrong callback', 'Parameter "onReady" provided to jsfly.createServer(..., onReady) must be a function.']
 
     //'new function': ['New Function', 'jsfly.wingify() must be an object.']
 };
