ef72a1d920ae955855d4649de977d6016e40ba16
Jorge Zaccaro <jorgezaccaro@gmail.com>
2013-12-07 22:39:18 -0500

    An example about migrating code authoritatively from one server to another and running it immediately.

---
+++ examples/example7_client.js
@@ -0,0 +1,31 @@
+// CODE THAT IS MIGRATED TO ANOTHER SERVER
+// The main purpose of JSFly is to give running code the ability to be sent 
+// to run on another server, either authoritatively or autonomously
+var jsfly = require('../jsfly');
+
+// You can supply any function like the ones discussed in the previous examples
+var wingified = jsfly.wingify(function myNameIs(jsfly) {
+    setInterval(function () {
+        console.log("Hello world!");
+    }, 1000);
+    // If the code is not migrated after 5.2 seconds, it crashes itself
+    setTimeout(function () {
+        console.log("Good bye world!");
+        jsfly.crash();
+    }, 5200);
+}).run();
+
+// Then, you can send the code to run on another server by calling the #fly method
+// and providing the server parameters in an object
+setTimeout(function () {
+    wingified.fly({ 
+        host: 'localhost',  // optional for localhost
+        port: '3600' 
+    });
+}, 3100);
+
+/*OUTPUT:
+Hello world!       // Every 1 second
+Hello world!
+Hello world!
+*/
---
+++ examples/example7_server.js
@@ -0,0 +1,32 @@
+// CODE THAT IS MIGRATED FROM ANOTHER SERVER
+// JSFly offers the #createAirport method that creates a server
+// that is capable of receiving code from another server and running it
+var jsfly = require('../jsfly');
+
+// The #createAirport method receives an options parameter with the desired port
+var options = {
+    port: '3600'
+};
+
+// A callback must be supplied to receive the 'airport' object 
+// in order to start handling JSFly events
+jsfly.createAirport(options, function (airport) {
+    // The 'ready' event is emitted when the server is created
+    airport.on('ready', function () {
+        console.log('JSFly airport ready.');
+    });
+    // The 'newJSPlane' event is emitted each time a new piece of code is received
+    airport.on('newJSPlane', function (jsPlane) {
+        console.log(jsPlane.name + ' is running here now!');
+    });
+});
+
+/*OUTPUT:
+myNameIs is running here now!
+Hello world!       // Every 1 second
+Hello world!
+Hello world!
+Hello world!
+Hello world!
+Good bye world!    // After 5.2 seconds 
+*/
