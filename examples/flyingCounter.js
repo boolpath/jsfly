02df21d9754df2c00dd8e9022126f606312478af
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-01-12 16:01:34 -0500

    An example of a counter that keeps its count when flying through a grid of servers.

--- examples/flyingCounter.js
+++ examples/flyingCounter.js
@@ -1,17 +1,20 @@
 var jsfly = require('../jsfly');
 
-var flyingCounter = jsfly.wingify(function myNameIs(jsfly, params) {
-    // Restore the count if the code is landing from another server
+var flyingCounter = jsfly.wingify(function flyingCounter(jsfly, params) {
+    /* Initialization stage */
+    // Restore the count value sent from the previous server if applicable
     var count = (params) ? params.count || 0 : 0;
     
     setInterval(function () {
         console.log(++count);
     }, 500);
     setTimeout(function () {
+        // Validate if there are more servers to visit
         if (params.servers.length > 0) {
             jsfly.fly({
                 port: params.servers.pop(),
-                // The current count must be sent as a parameter
+                // The current count must be sent as a parameter so that the counter is
+                // initialized when the code lands and starts running on another server
                 params: {
                     count: count,
                     servers: params.servers
