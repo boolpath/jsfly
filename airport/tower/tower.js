c5cc9f028f85568a95b2fb53ffd04cf7432283c3
Jorge Zaccaro <jorgezaccaro@gmail.com>
2014-01-14 23:37:05 -0500

    Added documentation and in-line comments.

--- airport/tower/tower.js
+++ airport/tower/tower.js
@@ -5,8 +5,11 @@ var net = require('net');
 var eventEmitter = require('events').EventEmitter;
 var tcpEventEmitter = require('../../utils/events/tcpEventEmitter');
 
-/** LOCAL OBJECT 
- * @property {} - 
+/** LOCAL OBJECT
+ * @property {string} host - The IP address where the JSFly airport/server is running
+ * @property {number} port - The port number the JSFly airport/server is listening to
+ * @property {object} server - Holds the TCP server object created with the #listen method
+ * @property {emitter} - The event emitter returned after creating the control tower
  */
 var TOWER = {
     host: undefined,
@@ -16,7 +19,7 @@ var TOWER = {
 };
 
 /** MODULE INTERFACE
- *@method {function} - 
+ *@method {function} listen - Creates a server that listens on a given port 
  */
 module.exports = {
     listen: listen
@@ -25,32 +28,33 @@ module.exports = {
 /*----------------------------------------------------------------------------*/
 
 /** Creates a TCP server to listen to other airport requests
- * @param {object} options - Options like the port to listen on
- * @param {function} onReady - A callback to be invoked when the server is ready
+ * @param {object} options - Options like the port to listen to
+ * @param {function} onReady - A callback to invoke when the server is ready
  * @returns
  */
 function listen(options, onReady) {
     TOWER.server = net.createServer(function onNewAirport(socket) {
         var newAirport = tcpEventEmitter.bind(socket);
         controlTraffic(newAirport);
-        console.log('New airport connected on port ' + socket.remotePort);
+        // console.log('New airport connected on port ' + socket.remotePort);
     });
     
     TOWER.server.listen(options.port, function () {
-        console.log('JSFly control tower listening on port ' + options.port);
+        // console.log('JSFly control tower listening on port ' + options.port);
         onReady(TOWER.emitter);
         TOWER.emitter.emit('ready');
     });
 }
 
 
-/** 
- * @param
- * @returns
+/** Handles communications and landing requests from other airports
+ * @param {object} clientAirport - The socket created to communicate with a client airport
  */
 function controlTraffic(clientAirport) {
+    // On incoming JSPlanes, allow them to land, notify the sender airport
+    // and emit the landing event for the airport to handle it
     clientAirport.on('jsPlane', function (jsPlane) {
-        console.log('New jsPlane comming:', jsPlane.name);
+        // console.log('New jsPlane comming:', jsPlane.name);
         clientAirport.send('landed', jsPlane.id);
         JSFly.airport.runway.land(jsPlane);
         TOWER.emitter.emit('landing', jsPlane);
